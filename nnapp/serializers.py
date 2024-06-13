78759b9c14850294c62091a35e508ab4c761caef
Alice <alice@DESKTOP-FVO98RR.Dlink>
2024-06-13 11:56:33 +0300

    add all

--- nnapp/serializers.py
+++ nnapp/serializers.py
@@ -1,13 +1,51 @@
-""" from rest_framework import serializers
-from .models import UploadedData, NeuralNetworkModel, Result
+from rest_framework import serializers
+from .models import UploadedData, NeuralNetwork, Result
+from django.contrib.auth.models import User
 
-class NNappSerializer(serializers.ModelSerializer):
+""" class DataSerializer(serializers.ModelSerializer):
+    
+    uploaded_by_name = serializers.SerializerMethodField()
+
     class Meta:
-        id = serializers.IntegerField(read_only=True)
-        title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-        code = serializers.CharField(style={'base_template': 'textarea.html'})
-        linenos = serializers.BooleanField(required=False)
-        language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-        style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+        model = UploadedData
+        fields = ('upload_date', 'uploaded_by_name', 'id', 'data_file')
 
- """
+    def get_uploaded_by_name(self, obj):
+        return obj.uploaded_by.username if obj.uploaded_by else None """
+
+class UploadedDataSerializer(serializers.Serializer):
+    data_file = serializers.FileField()
+    upload_date = serializers.DateTimeField(read_only=True)
+    uploaded_by = serializers.PrimaryKeyRelatedField(queryset=User.objects.all()) 
+
+    def create(self, validated_data):
+        return UploadedData.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.data_file = validated_data.get("data_file", instance.data_file)
+        instance.upload_date = validated_data.get("upload_date", instance.upload_date)
+        instance.uploaded_by = validated_data.get("uploaded_by", instance.uploaded_by)
+        instance.save()
+        return instance
+
+class NeuralNetworkSerializer(serializers.Serializer):
+    model_name = serializers.CharField(max_length=100)
+    description = serializers.CharField()
+    model_file = serializers.FileField(read_only=True)
+    upload_date = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        return NeuralNetwork.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.model_name = validated_data.get("model_name", instance.model_name)
+        instance.description = validated_data.get("description", instance.description)
+        instance.save()
+        return instance
+    
+
+
+""" class ResultSerializer(serializers.Serializer):
+    uploaded_data = serializers.PrimaryKeyRelatedField(queryset=UploadedData.objects.all())
+    neural_network_model = serializers.PrimaryKeyRelatedField(queryset=NeuralNetwork.objects.all())
+    result_values = serializers.CharField() """
--- nnapp/views.py
+++ nnapp/views.py
@@ -1,3 +1,72 @@
+from rest_framework import generics
 from django.shortcuts import render
+from .models import UploadedData
+from .serializers import UploadedDataSerializer
 
-# Create your views here.
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+
+class DataAPIView(APIView):
+
+    def get(self, request):
+        queryset = UploadedData.objects.all()
+        return Response({'data': UploadedDataSerializer(queryset, many =True).data})
+
+    
+    def post(self, request):
+            #проверка данных 
+        serializer = UploadedDataSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        """ post_new = UploadedData.objects.create(
+            data_file = request.data['data_file'],
+            uploaded_by = request.data['uploaded_by'],
+        ) """
+        return Response({'post': serializer.data})
+    
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({"error":"Method put not allowed"})
+        
+        try:
+            instanse = UploadedData.objects.get(pk=pk)
+        except:
+            return Response({"error":"Objects does not exist"})
+        
+        serializer = UploadedDataSerializer(data=request.data, instance=instanse)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"post":serializer.data})
+
+
+from .serializers import NeuralNetworkSerializer
+from .models import NeuralNetwork
+class NeuralNetworkAPIView(APIView):
+
+    def get(self, request):
+        queryset = NeuralNetwork.objects.all()
+        return Response({'data': NeuralNetworkSerializer(queryset, many =True).data})
+
+    
+    def post(self, request):
+            #проверка данных 
+        serializer = NeuralNetworkSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        """ post_new = NeuralNetwork.objects.create(
+
+            model_name = request.data['model_name'],
+            description = request.data['description'],
+        ) """
+        return Response({'post': serializer.data})
+
+
+
