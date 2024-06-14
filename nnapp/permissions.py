6c4169fb731339ffebdb09520e17bf9f83c92b7f
Alice <alice@DESKTOP-FVO98RR.Dlink>
2024-06-14 19:57:22 +0300

    add permissions classes

---
+++ nnapp/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework import permissions
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(request.uploaded_by and request.uploaded_by.is_staff)
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.uploaded_by == request.user
