67a559fc8a0f817d3d16f722fbbf350f409f2840
Alice00021 <alisa_barusheva03@mail.ru>
2024-06-16 15:49:25 +0300

    dont change models

--- nnapp/models.py
+++ nnapp/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class UploadedData(models.Model):
     data_file = models.FileField(upload_to='uploads/')
     upload_date = models.DateTimeField(auto_now_add=True)
-    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    uploaded_by = models.ForeignKey(User,  on_delete=models.CASCADE)
 
 class NeuralNetwork(models.Model):
     model_name = models.CharField(max_length=100)
@@ -16,6 +17,7 @@ class Result(models.Model):
     uploaded_data = models.ForeignKey(UploadedData, on_delete=models.CASCADE)
     neural_network_model = models.ForeignKey(NeuralNetwork, on_delete=models.CASCADE)
     result_values = models.TextField()
+
 
 
   
