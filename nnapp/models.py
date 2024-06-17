c730c9e2e8bf129b3c8d2009f47c0a2fb1e3958a
Alice00021 <alisa_barusheva03@mail.ru>
2024-06-17 14:49:38 +0300

    add uploaded_by to our three tables

--- nnapp/models.py
+++ nnapp/models.py
@@ -1,22 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.utils import timezone
 
 class UploadedData(models.Model):
     data_file = models.FileField(upload_to='uploads/')
-    upload_date = models.DateTimeField(auto_now_add=True)
-    uploaded_by = models.ForeignKey(User,  on_delete=models.CASCADE)
+    upload_date = models.DateTimeField(default=timezone.now)
+    uploaded_by = models.ForeignKey(User,  on_delete=models.CASCADE, default=None)
 
 class NeuralNetwork(models.Model):
     model_name = models.CharField(max_length=100)
     description = models.TextField()
     model_file = models.FileField(upload_to='models/')
-    upload_date = models.DateTimeField(auto_now_add=True)
+    upload_date = models.DateTimeField(default=timezone.now)
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+
 
 class Result(models.Model):
     uploaded_data = models.ForeignKey(UploadedData, on_delete=models.CASCADE)
     neural_network_model = models.ForeignKey(NeuralNetwork, on_delete=models.CASCADE)
     result_values = models.TextField()
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+   
 
 
 
