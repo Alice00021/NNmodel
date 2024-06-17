01da53f434b5c3db24bc75d21c6519ea476e0317
Alice00021 <alisa_barusheva03@mail.ru>
2024-06-17 14:51:22 +0300

    add class Userserializer

--- nnapp/serializers.py
+++ nnapp/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import UploadedData, NeuralNetwork, Result
 from django.contrib.auth.models import User
 
- 
 class UploadedDataSerializer(serializers.ModelSerializer):
     uploaded_by = serializers.HiddenField(default=serializers.CurrentUserDefault())
     class Meta:
@@ -10,12 +9,26 @@ class UploadedDataSerializer(serializers.ModelSerializer):
         fields = ['data_file', 'uploaded_by']
 
 class NeuralNetworkSerializer(serializers.ModelSerializer):
+    uploaded_by = serializers.HiddenField(default=serializers.CurrentUserDefault())
     class Meta:
         model = NeuralNetwork
         fields = ['model_name', 'description']
 
 
-class ResultSerializer(serializers.Serializer):
+class ResultSerializer(serializers.ModelSerializer):
+    uploaded_by = serializers.HiddenField(default=serializers.CurrentUserDefault())
     class Meta:
-        model = NeuralNetwork
-        fields = ['__all__']
+        model = Result
+        fields = '__all__'
+        # fields = ['uploaded_data', 'neural_network_model', 'result_values', 'uploaded_by']
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email', 'first_name', 'last_name']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
