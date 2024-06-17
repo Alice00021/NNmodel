6b0755e933af7e8cb937236205abf65c23df3c8f
Alice00021 <alisa_barusheva03@mail.ru>
2024-06-17 15:49:27 +0300

    try to add permissions

--- NNmodel/urls.py
+++ NNmodel/urls.py
@@ -1,15 +1,29 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework import routers
 from nnapp.views import *
-
-router = routers.SimpleRouter()
-router.register(r'datalist', UploadedDataViewSet)
-router.register(r'model', NeuralNetworkViewSet)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework_simplejwt.views import TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/',include(router.urls)),
-    path('api/v1/drf-auth/',include('rest_framework.urls'))
+    path('api/v1/datalist/',UploadedDataAPIList.as_view()),
+    path('api/v1/datalist/<int:pk>/',UploadedDataAPIUpdate.as_view()),
+    path('api/v1/datalistdelete/<int:pk>/',UploadedDataAPIDestroy.as_view()),
+    
+    path('api/v1/model/',NeuralNetworkAPIList.as_view()),
+    path('api/v1/model/<int:pk>/',NeuralNetworkAPIUpdate.as_view()),
+    path('api/v1/modeldelete/<int:pk>/',NeuralNetworkAPIDestroy.as_view()),
+
+    path('api/v1/result/',ResultAPIListDestroy.as_view()),
+
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    path('register/', UserCreate.as_view(), name='register'),
 ]
 
--- nnapp/views.py
+++ nnapp/views.py
@@ -1,25 +1,55 @@
 from rest_framework import generics
-from django.shortcuts import render
-from .models import UploadedData
-from .serializers import UploadedDataSerializer
-
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-from .serializers import NeuralNetworkSerializer
-from .models import NeuralNetwork
-
-from rest_framework import status, viewsets
-
-class UploadedDataViewSet(viewsets.ModelViewSet):
+from .models import UploadedData, NeuralNetwork, Result
+from .serializers import UploadedDataSerializer, NeuralNetworkSerializer, ResultSerializer
+from rest_framework import  viewsets
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import authentication, permissions
+from .permissions import *
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+#####################UploadedData###################################################
+class UploadedDataAPIList(generics.ListCreateAPIView):
     queryset = UploadedData.objects.all()
     serializer_class = UploadedDataSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
-class NeuralNetworkViewSet(viewsets.ModelViewSet):
+class UploadedDataAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = UploadedData.objects.all()
+    serializer_class = UploadedDataSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+class UploadedDataAPIDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UploadedData.objects.all()
+    serializer_class = UploadedDataSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+#####################NeuralNetwork###################################################
+class NeuralNetworkAPIList(generics.ListCreateAPIView):
     queryset = NeuralNetwork.objects.all()
     serializer_class = NeuralNetworkSerializer
 
+class NeuralNetworkAPIUpdate(generics.RetrieveUpdateAPIView):
+    queryset = NeuralNetwork.objects.all()
+    serializer_class = NeuralNetworkSerializer
 
-    
+class NeuralNetworkAPIDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = NeuralNetwork.objects.all()
+    serializer_class = NeuralNetworkSerializer
 
+#####################Result###################################################
+
+
+class ResultAPIListDestroy(generics.RetrieveDestroyAPIView):
+    queryset = Result.objects.all()
+    serializer_class = ResultSerializer
+
+#############################################################################################
+
+class UserCreate(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]  # Разрешаем доступ без аутентификации
+
+    def perform_create(self, serializer):
+        serializer.save()
 
